06703a864d8c13e6325f16c62f4afbb595ef85c6
Nazik <nvovk.2004@gmail.com>
2024-03-22 18:54:48 +0200

    feat: initial setup, add convex&clerk auth, dashboard layout

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const config = {
+  providers: [
+    {
+      domain: 'https://humorous-dinosaur-23.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
+
+export default config;
